1797a9c18918d807a085e8791016c33e7b592c66
Oursel06 <alexoursel6@gmail.com>
2023-10-15 00:00:56 +0200

    ajout vérification mail

--- js/script.js
+++ js/script.js
@@ -24,36 +24,46 @@ $("#envoyer").on("click", function () {
         $('#results').html("Veuillez remplir tous les champs.");
         $("#envoyer").removeClass("disable");
     } else {
-        $(".loader-form").removeClass("hidden");
-        $.ajax({
-            url: 'mailsend.php',
-            type: 'POST',
-            data: {
-                mail: mail,
-                sujet: sujet,
-                message: message
-            },
-            dataType: 'json',
-            success: function (result) {
-                $(".loader-form").addClass("hidden");
-                $(".message-form").removeClass("hidden");
-                if (result.status === 'success') {
-                    $("#results").html(result.message);
-                    $(".message-form").css("background", "limegreen");
-                    $("#envoyer").removeClass("disable");
-                    cleanForm();
-                } else {
-                    $("#results").html(result.message);
-                    $(".message-form").css("background", "red");
+        var regexEmail = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+        var isEmailValid = regexEmail.test(mail);
+
+        if (!isEmailValid) {
+            $(".message-form").removeClass("hidden");
+            $('.message-form').css("background", "red");
+            $('#results').html("L'adresse mail n'est pas valide.");
+            $("#envoyer").removeClass("disable");
+        } else {
+            $(".loader-form").removeClass("hidden");
+            $.ajax({
+                url: 'mailsend.php',
+                type: 'POST',
+                data: {
+                    mail: mail,
+                    sujet: sujet,
+                    message: message
+                },
+                dataType: 'json',
+                success: function (result) {
+                    $(".loader-form").addClass("hidden");
+                    $(".message-form").removeClass("hidden");
+                    if (result.status === 'success') {
+                        $("#results").html(result.message);
+                        $(".message-form").css("background", "limegreen");
+                        $("#envoyer").removeClass("disable");
+                        cleanForm();
+                    } else {
+                        $("#results").html(result.message);
+                        $(".message-form").css("background", "red");
+                        $("#envoyer").removeClass("disable");
+                    }
+                },
+                error: function (result) {
+                    $('#results').html(result.message);
+                    $('.message-form').css("background", "red");
                     $("#envoyer").removeClass("disable");
                 }
-            },
-            error: function (result) {
-                $('#results').html(result.message);
-                $('.message-form').css("background", "red");
-                $("#envoyer").removeClass("disable");
-            }
-        });
+            });
+        }
     }
 })
 
